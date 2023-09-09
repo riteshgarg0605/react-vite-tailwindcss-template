54c85b239c5a9fdc7cd71e87c7f3ca792cb25f35
Ritesh Garg <riteshgarg0605@gmail.com>
2023-09-09 14:00:07 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-center text-5xl font-bold">
+        'React + Vite + Tailwind CSS' Template
+      </p>
+    </>
+  );
+}
+
+export default App;
